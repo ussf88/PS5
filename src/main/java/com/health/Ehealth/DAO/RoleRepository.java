293f8853c9b5dad9cc414ead4c469e5ee7e7c951
youssef el faquir <youssef1998islam@gmail.com>
2021-01-12 23:49:26 +0100

    added jwt auth with user

---
+++ src/main/java/com/health/Ehealth/DAO/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.health.Ehealth.DAO;
+import java.util.Optional;
+import com.health.Ehealth.modal.*;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByName(ERole name);
+}
---
+++ src/main/java/com/health/Ehealth/modal/ERole.java
@@ -0,0 +1,7 @@
+package com.health.Ehealth.modal;
+
+public enum ERole {
+	ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
